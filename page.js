fba7379319d4e038d99e4d3c3342cc435453c1ba
9p-6 <khaled12131213khaled@gmail.com>
2025-06-01 06:32:25 +0300

    Add files via upload

---
+++ page.js
@@ -0,0 +1,39 @@
+function bgcolor() {
+    document.getElementById('al5alefya').style.background=document.getElementById('bgcolor').value;
+    
+}
+
+
+function login() {
+    const data = {
+        relon:{password:'1'},
+        osama:{password:'12345'},
+        hamza:{password:'1234'},
+        foad:{password:'123'},
+    }
+        const inpuserRaw = document.getElementById('inptext').value;
+        const inpuser = inpuserRaw.toLowerCase();
+        const inppass = document.getElementById('inppass').value;
+        const msg = document.getElementById('msg')
+
+    if (data.hasOwnProperty(inpuser) && inppass === data[inpuser].password) {
+        window.msg.innerHTML=`<span style='color:green;'> Welcome </span>` + inpuserRaw+"!"
+    
+        const authToken = Math.random().toString(36).substring(2);
+        sessionStorage.setItem('authToken', authToken);
+        window.location.href = "hhh.html";
+
+    } else {
+        window.msg.innerHTML=`<span style=
+        'color:red;'
+        font-size: 6vw;
+        >Check UserName Or Password</span>`
+    }
+  
+    
+}
+
+  function logout() {
+    sessionStorage.removeItem('authToken');
+    window.location.href = "index.html";
+}
